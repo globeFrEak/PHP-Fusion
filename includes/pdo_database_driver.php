8a6c42a5e8d92ce533d1e456e0330b2e38d03843
globeFrEak <globefreak@gmail.com>
2016-06-27 20:29:10 +0200

    PHPDoc
    PHPDoc lines added for PDO functions

--- includes/pdo_database_driver.php
+++ includes/pdo_database_driver.php
@@ -22,6 +22,15 @@ if (!defined("IN_FUSION")) {
     die("Access Denied");
 }
 
+/**
+ * Opens or re-uses a connection to a MySQL server.
+ * @param string $db_host The MySQL server. It can also include a port number. e.g. "hostname:port" or a path to a local socket e.g. ":/path/to/socket" for the localhost.
+  If the PHP directive mysql.default_host is undefined (default), then the default value is 'localhost:3306'. In SQL safe mode, this parameter is ignored and value 'localhost:3306' is always used.
+ * @param string $db_user The username. Default value is defined by mysql.default_user. In SQL safe mode, this parameter is ignored and the name of the user that owns the server process is used.
+ * @param string $db_pass The password. Default value is defined by mysql.default_password. In SQL safe mode, this parameter is ignored and empty password is used.
+ * @param string $db_name The name of the database that is to be selected.
+ * @return mixed Returns a PDO object on success or an Excpetion on failure
+ */
 function dbconnect($db_host, $db_user, $db_pass, $db_name) {
     global $pdo;
     try {
@@ -34,6 +43,12 @@ function dbconnect($db_host, $db_user, $db_pass, $db_name) {
     }
 }
 
+/**
+ * dbquery() sends an unique query to the database.
+ * @param resource $query A SQL query, The query string should not end with a semicolon.
+ * @param array $execute
+ * @return mixed FALSE or an array $result
+ */
 function dbquery($query, $execute = array()) {
     global $pdo, $mysql_queries_count, $mysql_queries_time;
     $mysql_queries_count++;
@@ -50,6 +65,10 @@ function dbquery($query, $execute = array()) {
     }
 }
 
+/**
+ * @param resource $query A SQL query, The query string should not end with a semicolon.
+ * @return array $result 
+ */
 function dbquery_exec($query) {
     global $pdo, $mysql_queries_count, $mysql_queries_time;
     $mysql_queries_count++;
@@ -60,6 +79,15 @@ function dbquery_exec($query) {
     return $result;
 }
 
+/**
+ * Counts records by $field (can be all fields if this value is (*) ) from $table in some $conditions (default no conditions) written in SQL syntax.
+ * @param string $field The fields you want to count in the database
+ * @param string $table The table you want to count from
+ * @param string $conditions The conditions you want to the results to match
+ * @param array $execute
+ * @return mixed This function will return the number of rows in the $table matching the $conditions if any.
+This function will return false if there are no rows or on failure.
+ */
 function dbcount($field, $table, $conditions = "", $execute = array()) {
     global $pdo, $mysql_queries_count, $mysql_queries_time;
     $mysql_queries_count++;
@@ -77,6 +105,12 @@ function dbcount($field, $table, $conditions = "", $execute = array()) {
     }
 }
 
+/**
+ * Retrieves the contents of one cell from a MySQL result set.
+ * @param resource $query The query resource that is being evaluated. This result comes from a call to dbquery().
+ * @param string $row The row number from the result that's being retrieved. Row numbers start at 0.
+ * @return array $result The contents of one cell from a MySQL result set on success, or FALSE on failure.
+ */
 function dbresult($query, $row) {
     global $pdo, $mysql_queries_count, $mysql_queries_time;
     $query_time = get_microtime();
@@ -92,28 +126,53 @@ function dbresult($query, $row) {
     }
 }
 
+/**
+ * This function will give you the total number of rows in a given database query. 
+ * This is often used to check for rows in a result before displaying content.
+ * @param resource $query
+ * @return mixed The number of rows as int, or FALSE on failure.
+ */
 function dbrows($query) {
     return $query->rowCount();
 }
 
+/**
+ * Returns an associative array that corresponds to the fetched row and moves the internal data pointer ahead. * 
+ * @param resource $query
+ * @return array Returns an associative array of strings that corresponds to the fetched row, or FALSE if there are no more rows.
+ */
 function dbarray($query) {
     global $pdo;
     $query->setFetchMode(PDO::FETCH_ASSOC);
     return $query->fetch();
 }
 
+/**
+ * Returns a numerical array that corresponds to the fetched row and moves the internal data pointer ahead. 
+ * @param resource $query The result resource that is being evaluated. This result comes from a call to dbquery().
+ * @return array Returns an numerical array of strings that corresponds to the fetched row, or FALSE if there are no more rows. 
+ */
 function dbarraynum($query) {
     global $pdo;
     $query->setFetchMode(PDO::FETCH_NUM);
     return $query->fetch();
 }
 
+/**
+ * Returns the ID of the last inserted row or sequence value
+ * @return string $id Returns the ID of the last inserted row, or the last value from a sequence object, depending on the underlying driver.
+ */
 function dblastid() {
     global $pdo;
     $id = $pdo->lastInsertId();
     return $id;
 }
 
+/**
+ * Check if a table exists
+ * @param string $table
+ * @return boolean If table exists (TRUE) or not (FALSE)
+ */
 function dbexists($table) {
     global $pdo;
 
@@ -123,8 +182,9 @@ function dbexists($table) {
     }
     if (dbrows($results) > 0) {
         return true;
-    } else
+    } else {
         return false;
+    }
 }
 
 ?>
