5b70436543f3ae16679d828172c96269b6263950
Patrick Conrad <conrad@iza.org>
2016-07-11 15:09:14 +0200

    Update mail and url bbcode

--- includes/bbcodes/mail_bbcode_include.php
+++ includes/bbcodes/mail_bbcode_include.php
@@ -5,7 +5,7 @@
 | https://www.php-fusion.co.uk/
 +--------------------------------------------------------+
 | Filename: mail_bbcode_include.php
-| Author: Wooya
+| Author: JoiNNN
 +--------------------------------------------------------+
 | This program is released as free software under the
 | Affero GPL license. You can redistribute it and/or
@@ -15,8 +15,36 @@
 | copyright header is strictly prohibited without
 | written permission from the original author(s).
 +--------------------------------------------------------*/
-if (!defined("IN_FUSION")) { die("Access Denied"); }
+if (!defined("IN_FUSION")) {
+    die("Access Denied");
+}
 
-$text = preg_replace('#\[mail\]([\r\n]*)([^\s\'\";:\+]*?)([\r\n]*)\[/mail\]#sie', "hide_email('\\2').''", $text);
-$text = preg_replace('#\[mail=([\r\n]*)([^\s\'\";:\+]*?)\](.*?)([\r\n]*)\[/mail\]#sie', "hide_email('\\2').''", $text);
-?>
+if (!function_exists('replace_mail')) {
+    function replace_mail($m)
+    {
+        $mail    = !empty($m['mail']) ? $m['mail'] : (!empty($m['mail2']) ? $m['mail2'] : $m[0]);
+        $subject = !empty($m['subject']) ? $m['subject'] : '';
+        $title   = !empty($m['title']) ? $m['title'] : $mail;
+
+        return hide_email($mail, $title, $subject);
+    }
+}
+
+$mail_regex = '[-0-9A-Z_\.]{1,50}@([-0-9A-Z_\.]+\.){1,50}([0-9A-Z]){2,4}';
+if (isset($text)) {
+    $text = preg_replace_callback('
+~\[mail
+(=
+	(?P<mail>'.$mail_regex.')
+	(;(?P<subject>.*?))?
+)?
+\]
+(?(?='.$mail_regex.') # if followed by
+	(?P<mail2>'.$mail_regex.') # then
+	|
+	(?P<title>.*?)? # else
+)
+\[\/mail\]
+~ix'
+        , 'replace_mail', $text);
+}
